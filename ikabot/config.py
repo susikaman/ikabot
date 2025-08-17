ed7f1bbbd20b5f9fb7e1867d244b5a64f7b8438a
susman <61547860+susikaman@users.noreply.github.com>
2025-08-18 02:18:22 +0300

    Build date update

--- ikabot/config.py
+++ ikabot/config.py
@@ -6,7 +6,7 @@ import os
 
 # Version changed automatically by the release pipeline
 IKABOT_VERSION = "7.1.12"
-BUILD_DATE = "8.8.2025"
+BUILD_DATE = "18.8.2025"
 
 
 IKABOT_VERSION_TAG = "v" + IKABOT_VERSION + " - " + BUILD_DATE
